1620f34a80b65da22e810074540c85ab0633627f
David Njungo <davidnjungo3@gmail.com>
2021-06-14 16:46:29 +0300

    add the if and else if date conditions

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ function getData() {
     var MM = parseInt(document.getElementById("mname").value)
     var CC = parseInt(document.getElementById("cname").value)
     var YY = parseInt(document.getElementById("yname").value)
+    if (DD < 1 || DD > 31) {
+        alert("Enter a valid day");
+    }
+    else if (MM < 1 || MM > 12) {
+        alert("Enter a valid month")
+    }
+    else if (CC < 1 || CC > 20) {
+        alert("Enter a valid century")
+    }
+    else if (YY < 0 || YY > 21) {
+        alert("Enter a valid year")
+    }
