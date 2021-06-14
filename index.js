3f70027dd18452782450d162a1f19d4dc0526bbf
David Njungo <davidnjungo3@gmail.com>
2021-06-14 16:43:51 +0300

    add getData function and assign variables

---
+++ index.js
@@ -0,0 +1,5 @@
+function getData() {
+    var DD = parseInt(document.getElementById("dname").value)
+    var MM = parseInt(document.getElementById("mname").value)
+    var CC = parseInt(document.getElementById("cname").value)
+    var YY = parseInt(document.getElementById("yname").value)
